42e507f84bd42fe0862df2dc0d7b0046897ddbf6
Arthur Cosentino <arthurcose@gmail.com>
2017-07-10 21:15:49 -0400

    Add Scorpion

---
+++ test/src/scorpion.cpp
@@ -0,0 +1,29 @@
+#include "engine.hpp"
+
+class Scorpion : public Prova::Entity
+{
+  Prova::Sprite sprite;
+
+  enum Animations {
+    WALK
+  };
+
+  public:
+    Scorpion()
+      : sprite("assets/scorpion.png", 8, 4)
+    {
+      sprite.CreateAnimation(Animations::WALK, 0, 2, .2f);
+      sprite.PlayAnimation(Animations::WALK, true);
+      sprite.origin.x = 4;
+      sprite.origin.y = 4;
+    }
+    void Draw(Prova::Screen& screen)
+    {
+      float offsetY = screen.GetHeight() / 2 + scene->camera.position.y;
+
+      float range = scene->camera.zFar - scene->camera.zNear;
+      position.z = (position.y + offsetY) / range;
+
+      screen.DrawSprite(sprite, position);
+    }
+};
