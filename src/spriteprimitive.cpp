fe9dc5feb6e68ec29d73883b7c3da054a98e5fcb
Arthur <arthurcose@gmail.com>
2017-07-22 10:07:26 -0400

    Fix SpritePrimitive constructor

--- src/spriteprimitive.cpp
+++ src/spriteprimitive.cpp
@@ -17,12 +17,14 @@ SpritePrimitive::SpritePrimitive()
 SpritePrimitive::SpritePrimitive(std::string sheetpath, int width, int height)
   : texture(sheetpath)
 {
-  SpritePrimitive();
-  
   this->width = width;
   this->height = height;
+  clip.left = 0;
+  clip.top = 0;
   clip.width = width / (float) texture.width;
   clip.height = height / (float) texture.height;
   origin.x = width / 2;
   origin.y = height / 2;
+  scale.x = 1;
+  scale.y = 1;
 }
