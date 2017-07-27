2c09d13e5898dd35c42dbead46989ee7e76a62f8
Arthur <arthurcose@gmail.com>
2017-07-27 10:47:21 -0400

    Add logging for Vectors and Rects

--- include/logging.hpp
+++ include/logging.hpp
@@ -1,8 +1,16 @@
 #pragma once
 #include <string>
+#include "vector4.hpp"
+#include "vector3.hpp"
+#include "vector2.hpp"
+#include "rect.hpp"
 
 namespace Prova
 {
   void Log(float number);
+  void Log(Vector2 vector);
+  void Log(Vector3 vector);
+  void Log(Vector4 vector);
+  void Log(Rect rect);
   void Log(std::string message);
 }
--- src/logging.cpp
+++ src/logging.cpp
@@ -2,12 +2,70 @@
 #include <iostream>
 #include <string>
 #include "logging.hpp"
+#include "vector4.hpp"
+#include "vector3.hpp"
+#include "vector2.hpp"
+#include "rect.hpp"
 
 using namespace Prova;
 
 void Prova::Log(float number)
 {
   Log(std::to_string(number));
+}
+
+void Prova::Log(Vector2 vector)
+{
+  std::string message = "Vector2(";
+  message += std::to_string(vector.x);
+  message += ",";
+  message += std::to_string(vector.y);
+  message +=  ")";
+  
+  Log(message);
+}
+
+void Prova::Log(Vector3 vector)
+{
+  std::string message = "Vector3(";
+  message += std::to_string(vector.x);
+  message += ",";
+  message += std::to_string(vector.y);
+  message += ",";
+  message += std::to_string(vector.z);
+  message +=  ")";
+  
+  Log(message);
+}
+
+void Prova::Log(Vector4 vector)
+{
+  std::string message = "Vector4(";
+  message += std::to_string(vector.x);
+  message += ",";
+  message += std::to_string(vector.y);
+  message += ",";
+  message += std::to_string(vector.z);
+  message += ",";
+  message += std::to_string(vector.w);
+  message +=  ")";
+
+  Log(message);
+}
+
+void Prova::Log(Rect rect)
+{
+  std::string message = "Rect(";
+  message += std::to_string(rect.left);
+  message += ",";
+  message += std::to_string(rect.top);
+  message += ",";
+  message += std::to_string(rect.width);
+  message += ",";
+  message += std::to_string(rect.height);
+  message +=  ")";
+
+  Log(message);
 }
 
 void Prova::Log(std::string message)
