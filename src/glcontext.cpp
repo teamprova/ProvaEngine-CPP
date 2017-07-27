5f5d133a58c97cec83a7f271caa9f9bb981dded8
Arthur <arthurcose@gmail.com>
2017-07-26 23:12:51 -0400

    Remove GLEW fluff

--- src/glcontext.cpp
+++ src/glcontext.cpp
@@ -17,14 +17,11 @@ GLContext::GLContext(void* window)
   }
   
   //Initialize GLEW
-  glewExperimental = GL_TRUE; 
   GLenum glewError = glewInit();
 
   if(glewError != GLEW_OK)
   {
-    //const GLubyte can't be used as a c++ string for some reason
-    //std::string error(glewGetErrorString(glewError));
-    throw std::runtime_error("GLEW Error");// + error;
+    throw std::runtime_error("Failed to initialize GLEW");
   }
 }
 
